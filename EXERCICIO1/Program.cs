1126371cbf040c831a20b9a87d46e02a099ba129
luazrk <luazrk@gmail.com>
2019-10-08 22:15:59 -0300

    novo projeto de c#

---
+++ EXERCICIO1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCICIO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior, menor;
+
+
+
+
+            Console.Write("Receber n1 do usuario: ");
+            maior = int.Parse(Console.ReadLine());
+            Console.Write("Receber n2 do usuario: ");
+            menor = int.Parse(Console.ReadLine());
+            Console.WriteLine("A diferença de n1: " + maior + " e n2: " + menor + " é: " + diferenca(maior, menor));
+            Console.ReadKey();
+        }
+        static int diferenca(int maior, int menor)
+        {
+            string D = "";
+            if (maior > menor)
+                return maior - menor;
+            else
+                return menor - maior;
+        }
+
+    }
+}
+}
